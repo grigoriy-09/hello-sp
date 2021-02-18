05f5d722721e218d606d42e5c77ab9a94efe5b68
grigoriy-09 <74082688+grigoriy-09@users.noreply.github.com>
2021-02-18 05:35:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,5 +8,5 @@ terraform {
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
   #value = "Hello, World!"
-  value = var.users[0]
+  value = "${var.congr}"
 }
